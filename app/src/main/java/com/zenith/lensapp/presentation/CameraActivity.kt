79a0c7cc9e1260d89b1cd48f04686fe5b9edb83d
Ilya <ilya.trubmet@yandex.ru>
2020-01-22 14:45:37 +0300

    Add GLSurfaceView with Custom Renderer

--- app/src/main/java/com/zenith/lensapp/presentation/CameraActivity.kt
+++ app/src/main/java/com/zenith/lensapp/presentation/CameraActivity.kt
@@ -1,19 +1,25 @@
 package com.zenith.lensapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.zenith.lensapp.R
+import com.zenith.lensapp.presentation.glview.PreviewGLRenderer
+import com.zenith.lensapp.presentation.glview.TextureRenderer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class CameraActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CameraViewModel
+    private lateinit var renderer: PreviewGLRenderer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +28,18 @@ class CameraActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this).get(CameraViewModel::class.java)
         initShutterAnimation()
+        initFilterChanger()
+        initCameraPreview()
     }
+
+    fun initCameraPreview(){
+        renderer = PreviewGLRenderer(effectsview)
+        effectsview.setEGLContextClientVersion(2)
+        effectsview.setRenderer(renderer)
+        effectsview.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+        renderer.setImage(BitmapFactory.decodeResource(getResources(), R.drawable.puppy))
+    }
+
 
     private fun setFullscreenMode(){
         this.window.setFlags(
@@ -35,6 +52,24 @@ class CameraActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+
+    private fun initFilterChanger(){
+        container.setOnTouchListener(object: OnSwipeListener(this){
+            override fun onSwipeRight() {
+                viewModel.previousFilter()
+            }
+
+            override fun onSwipeLeft() {
+                viewModel.nextFilter()
+            }
+
+        })
+
+        viewModel.currentFilter.observe(this, Observer{
+            //Toast.makeText(this, "Value is $it", Toast.LENGTH_SHORT).show()
+            renderer.setEffect(it)
+        })
     }
 
     private fun initShutterAnimation(){
--- app/src/main/java/com/zenith/lensapp/presentation/CameraViewModel.kt
+++ app/src/main/java/com/zenith/lensapp/presentation/CameraViewModel.kt
@@ -8,11 +8,38 @@ class CameraViewModel : ViewModel(){
         MutableLiveData<Boolean>(false)
     }
 
+    val currentFilter by lazy {
+        MutableLiveData(0)
+    }
+
     fun releaseShutter(){
         isShutterLongPressed.postValue(false)
     }
 
     fun touchShutter(){
         isShutterLongPressed.postValue(true)
+    }
+
+    fun nextFilter(){
+            currentFilter.postValue(setFilter(currentFilter.value, 1))
+    }
+
+    fun previousFilter(){
+        currentFilter.postValue(setFilter(currentFilter.value, -1))
+    }
+
+    private fun setFilter(value: Int?, step: Int): Int{
+        if(value == null){
+            return 0
+        }else{
+            val result = value + step
+
+            if(result < 0){
+                return 3
+            }else if(result > 3){
+                return 0
+            }
+            return result
+        }
     }
 }
